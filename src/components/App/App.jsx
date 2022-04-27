ba69ff7f466141df10d6495f49fea7a92ecf2f54
Vyacheslav <dikey677@gmail.com>
2022-04-27 22:02:00 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,14 +10,15 @@ import useLocalStorage from "../../Hooks/useLocalStorageApp";
 
 export default function App() {
   const [filter, setFilter] = useState('')
-  const [contacts, setContacts] = useLocalStorage('contacts', [ 
+
+  const phonebookName = [ 
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ])
-    
+  ]
   
+  const [contacts, setContacts] = useLocalStorage('contacts', phonebookName)
 
   const formSubmitHandler = onSubmit => {
     console.log(onSubmit)
